a51bd4e466cedc38dacf7a046813b7a6d4f972d7
Nana <72888369+nanaama527@users.noreply.github.com>
2021-01-23 20:08:04 -0500

    commit tree 1bf3b1227707d8e3b514c4b5a7d1aa1ef899c583

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,28 @@
 def oxford_comma(array)
-   array = "kiwi"
-   return array
+   if array.length == 1
+   return array.join
+   elsif array.length == 2
+    return array.join(" and ")
+   else
+   last_element = array.pop 
+    return array.join(", ") << ", and #{last_element}" 
+end
 end
 
-def oxford_comma(array)
-    array = ["kiwi", "durian"]
-    array.reduce(:+)
-    return array
-end
 
-def oxford_comma (array)
-    array = ["Kiwi", "durian", "starfruit"]
-    array.map.insert("and") 
-    return array
-end
+
+
+
+
+
+#  def oxford_comma(array)
+#     array = ["kiwi", "durian"]
+#     array.reduce(:+)
+#     return array
+# end
+
+# def oxford_comma (array)
+#     array = ["Kiwi", "durian", "starfruit"]
+#     array.map.insert("and") 
+#     return array
+# end
